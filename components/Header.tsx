0a2c3aa905589d5b9c2e708da68f55cb5ff35bc7
Froggiy <155865426+Froggiy@users.noreply.github.com>
2024-07-14 09:44:19 +0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -98,6 +98,7 @@ const Header: React.FC<HeaderProps> = ({
                 </div>
                 <div className="flex md:hidden gap-x-2 items-center">
                     <button
+                    onClick={() => router.push("/")}
                     className="
                     rounded-full
                     p-2
@@ -112,6 +113,7 @@ const Header: React.FC<HeaderProps> = ({
                         <HiHome className="text-black" size={20}/>
                     </button>
                     <button
+                    onClick={() => router.push("/search")}
                     className="
                     rounded-full
                     p-2
